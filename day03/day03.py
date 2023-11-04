301a15ffd344ca563c41d73d0e520a77588f24da
Bronco Oostermeyer <dev@bfv.io>
2023-11-04 11:40:08 +0100

    day 3b

--- day03/day03.py
+++ day03/day03.py
@@ -3,10 +3,12 @@ def main():
     f = open("day03/input.txt", mode="r")
     rucksacks = f.read().split(sep="\n")
 
-    a, b = 0, 0
-    for rucksack in rucksacks:
+    a, b, i = 0, 0, 0
+    for i, rucksack in enumerate(rucksacks):
         part1, part2 = rucksack[:len(rucksack)//2], rucksack[len(rucksack)//2:]
         a += calc_char_values(set(part1).intersection(part2))
+        if (i+1) % 3 == 0:
+            b += calc_char_values(set(rucksacks[i]).intersection(rucksacks[i-1]).intersection(rucksacks[i-2]))
 
     print(f"a: {a}\nb: {b}")
 
